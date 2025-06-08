9f6d5fa140207a759fdcadbad483dbb2c6f47fa6
Raspeball <Raspeball@users.noreply.github.com>
2025-06-08 21:30:26 +0200

    Rename files

---
+++ test_petinfo.py
@@ -0,0 +1,25 @@
+#
+import ex-1_chp-10 as petinfo
+#
+#
+#
+# --- Main program --- #
+def main():
+    # user input
+    name = input("Enter the name of your pet: ")
+    print(f"You entered {name} as the name of your pet")
+
+    age = float(input("Enter the age (numbers only) of your pet: "))
+    print(f"You entered {age} as the age of your pet")
+
+    pet_type = input("Enter the type of your pet (dog, bird, snake, cat, etc.): ")
+    print(f"You entered {pet_type} as the type of your pet")
+    
+    # create pet object
+    pet = petinfo.Pet(name, age, pet_type)
+    
+    # Display pet information
+    print(pet)
+
+# call main function
+main()
